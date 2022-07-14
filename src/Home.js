87a8d91eaefa34e618a42d44bfc80c1231545ba2
Onyx Valencia <200004@utags.edu.mx>
2022-07-14 00:16:44 -0500

    Feat:Adding module Info and more styles

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App'
+import I from './Info'
 
 
 const Begin = () => {
@@ -11,7 +12,7 @@ const Begin = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path="/infopokemons" element={<App />}></Route>
-                    
+                    <Route path="/about" element={<I />}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ src/Info.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const I = () => {
+    return (
+        <div className='about'>
+            <div>
+                <img className="Imagen" src="./Onyx.webp" alt="perfil"></img>
+            </div>
+            <div className='about-me'>
+                <div>Universidad Tecnológica de Aguascalientes</div>
+                <div>Ingeniería en Desarrollo y Gestión de Software</div>
+                <div>Diseño Web Profesional</div>
+                <div>Onyx Ricardo Rios Valencia </div>
+                <div>Matricula: 200004</div>
+            </div>
+        </div>
+    );
+};
+export default I;
