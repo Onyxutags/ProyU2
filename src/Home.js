946dcbb0e65235a4d12c52d8cbd4bc56f3df01b8
Onyx Valencia <200004@utags.edu.mx>
2022-07-14 00:33:43 -0500

    Feat:Adding module Nav and more and finish styles

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App'
 import I from './Info'
-
+import Moving from './Nav'
 
 const Begin = () => {
 
     return (
         <div>
-          
+          < Moving/>
             <BrowserRouter>
                 <Routes>
                     <Route path="/infopokemons" element={<App />}></Route>
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Moving = () => {
+
+    return (
+        <nav>
+            <div className='options'>
+                <a href="/infopokemons" className='hover-underline-animation'> Pokemons </a>
+                <a href="/about" className='hover-underline-animation'> About Me </a>
+            </div>
+        </nav>
+    );
+
+};
+
+export default Moving;
